6e7c150cdc09e050f9a363f7bafa8a94b003ba14
Muhammad Hasnain Khatri Attari <hasnainkhatri45@gmail.com>
2024-05-27 04:31:30 -0700

    hn

---
+++ app.js
@@ -0,0 +1,20 @@
+
+// getting elements
+
+var img = document.getElementById('image')
+var leftButton = document.getElementById('left')
+var rightButton = document.getElementById('right')
+var array = ['./img/1.jpg', './img/3.avif', './img/2.jpg']
+var i = 0
+var j = 3
+function rightbutton() {
+    var formula = i++ % array.length
+    img.src = array[formula]
+
+}
+function leftbutton() {
+    var forra = j-- % 0
+    img.src = array[forra]
+
+}
+// setInterval(leftbutton, 1000)
